d1300fb43d4e3ead772443975449d2d884d9050d
Paulo S. Garcia <pgarciapydev@gmail.com>
2023-06-26 10:24:09 -0300

    Teste 2 para zerar o task list

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 
 @app.route("/", methods=["POST", "GET"])
 def index():
+    task_list = []
     if request.method == "POST":
         if request.form["task"].strip() != "":
             task_list.append(request.form["task"])
-        print(task_list)
         return redirect(url_for("index", tasklist = task_list))
     return render_template("index.html", tasklist = task_list)
 
@@ -17,5 +17,4 @@ def index():
 
 
 if __name__ == "__main__":
-    task_list = []
     app.run(debug=True, port=4000)
