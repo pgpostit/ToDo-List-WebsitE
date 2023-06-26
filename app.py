67978f71fa731213bbed239a0b983b9e62f5cec9
Paulo S. Garcia <pgarciapydev@gmail.com>
2023-06-26 10:54:24 -0300

    Solução do reset da tasklist

--- app.py
+++ app.py
@@ -2,19 +2,20 @@ from flask import Flask, redirect, render_template, url_for, request
 
 app = Flask(__name__)
 
-
+task_list = []  # Lista de tarefas
 
 @app.route("/", methods=["POST", "GET"])
 def index():
-    task_list = []
     if request.method == "POST":
         if request.form["task"].strip() != "":
             task_list.append(request.form["task"])
-        return redirect(url_for("index", tasklist = task_list))
+        return redirect(url_for("tasks"))
+    task_list.clear()
+    return render_template("index.html")
+
+@app.route("/tasklist")
+def tasks():
     return render_template("index.html", tasklist = task_list)
 
-
-
-
 if __name__ == "__main__":
-    app.run(debug=True, port=4000)
+    app.run()
